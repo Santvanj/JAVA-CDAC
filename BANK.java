d8ce9ae3947282c8d944e577ffb724ccc16836a0
Santvanj <santvanj0808@gmail.com>
2025-03-14 10:11:47 +0530

    Update BANK.java

--- BANK.java
+++ BANK.java
@@ -2,8 +2,8 @@ package Inheritance;
 
 public class BANK {
     public static void main(String[] args) {
-        Customer regularCustomer = new Customer(101, "Alice", 50000);
-        ClassicCustomer classicCustomer = new ClassicCustomer(102, "Bob", 2000000);
+        Customer regularCustomer = new Customer(101, "Tom", 1000000);
+        ClassicCustomer classicCustomer = new ClassicCustomer(102, "Jerry", 1000000);
 
         System.out.println("Regular Customer Details:");
         regularCustomer.dispCustomer();
